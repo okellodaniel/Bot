00589a3949b08607eb5e330d2ed29a0de771d302
Daniel Okello <43291086+okellodaniel@users.noreply.github.com>
2023-08-02 08:37:28 +0300

    managed to get a step further

--- Booking/booking.py
+++ Booking/booking.py
@@ -1,9 +1,10 @@
-import types
-import typing
+
 from selenium import webdriver
 import Booking.constants as const
 import os
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from colorama import Fore, Back, Style
 
 class Booking(webdriver.Edge):
     def __init__(self,driver_path=os.getcwd(),teardown=False):
@@ -11,7 +12,7 @@ class Booking(webdriver.Edge):
         self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Booking,self).__init__()
-        self.implicitly_wait(15)
+        self.implicitly_wait(10)
         self.maximize_window()
 
     def landing_first_page(self):
@@ -33,24 +34,30 @@ class Booking(webdriver.Edge):
         """
         try:
             but = self.find_element(By.CSS_SELECTOR, 'button[aria-label="Dismiss sign in information."]')
+            print(but.get_attribute())
             but.click()
-        except Exception:
+        except Exception as e:
+            print(Fore.RED,e)
             pass
 
     def select_currency(self,currency=None):
         """
         Opens the currency selection dropdown and selects the desired currency.
         """
-        currency_element = self.find_element(By.CSS_SELECTOR,'button[data-testid="header-language-picker-trigger"]')
+        currency_element = self.find_element(By.CSS_SELECTOR,'button[data-testid="header-currency-picker-trigger"]')
+        actions = ActionChains(self)
+        actions.move_to_element(currency_element).click().perform()
         currency_element.click()
 
         if currency:
-        # select_currency_element = self.find_element(By.CSS_SELECTOR,f'button[data-testid="selection-item={currency}"]')
             try:
-                select_currency_element = self.find_element(By.CLASS_NAME,"ea1163d21f")
-                select_currency_element.click()
-            except Exception:
-                 print(f"Currency '{currency}' not found. Keeping default currency.")
+                select_currency_element = self.find_elements(By.CSS_SELECTOR,f'button[data-testid="selection-item"]')
+                for i in select_currency_element:
+                    select_currency = i.find_element(By.CLASS_NAME,"ea1163d21f")
+                    if select_currency.text == f"{currency}":
+                        i.click()
+            except Exception as e:
+                 print(f"Currency '{currency}' not found. Keeping default currency.",e)
         else:
             print("No currency provided. Keeping default currency.")
 
